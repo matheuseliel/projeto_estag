af386199c64c3b6bad6fa816fc9c6e6656cb0b0e
Matheus <matheuseliel100@hotmail.com>
2018-02-20 14:52:15 -0300

    att de segurança

--- PhpProject1/index.php
+++ PhpProject1/index.php
@@ -6,19 +6,54 @@
     </head>
     <body>
         <?php
-            $json_file = file_get_contents("https://jsonplaceholder.typicode.com/users");   
-            $json_str = json_decode($json_file, true);
-            $num_array = count($json_str);
+            class Dados{
+                
+                private $file;
+                private $website;
+                private $user;
+                private $email;
+                private $lat;
+                public $num;
+                
+                function setFile(){
+                    $json_file = file_get_contents("https://jsonplaceholder.typicode.com/users");   
+                    $json_str = json_decode($json_file, true);
+                    $contador3 = 0;
+                    $num_array = count($json_str);
+                    $this->num = $num_array;
+                    while($num_array != $contador3){
+                        $file = $json_str[$contador3];
+                        $this->website[$contador3] = $file['website'];
+                        $this->user[$contador3] = $file['username'];
+                        $this->email[$contador3] = $file['email'];
+                        $this->lat[$contador3] = $file['address']['geo']['lat'];
+                        $contador3 += 1;
+                    }
+                    return $this->file = $json_str;
+                }
+                function getWebsite($contador){
+                    return $this->website[$contador];
+                }
+                function getUser($contador1){
+                    return $this->user[$contador1];
+                }
+                function getEmail($contador1){
+                    return $this->email[$contador1];
+                }
+                function getLat($contador){
+                    return $this->lat[$contador];
+                }
+            }
+            
+            $contas = new Dados();
+            $contas->setFile();
             $contador = 0;
             $contador_sul = 0;
             echo "<a style='font-family: arial;'>Websites:<br>";
-            while($contador != $num_array){
-                $item_ext = $json_str[$contador];
-                $website = $item_ext['website'];
-                $address = $item_ext['address'];
-                $address2 = $address['geo'];
-                $lat = $address2['lat'];
+            while($contador != $contas->num){
+                $website = $contas->getWebsite($contador);
                 echo $website."<br>";
+                $lat = $contas->getLat($contador);
                 if($lat < 0){
                     $contador_sul += 1;
                 }
@@ -28,9 +63,8 @@
             $username = "";
         
             while($username != "Samantha"){
-                $item_ext = $json_str[$contador1];
-                $username = $item_ext['username'];
-                $email = $item_ext['email'];
+                $username = $contas->getUser($contador1);
+                $email = $contas->getEmail($contador1);
                 $contador1 += 1;
             }
             echo "<br>Email:<br>".$email."<br>";
